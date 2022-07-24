26c011bfb9db027feb36b6efff1fe0a21031b365
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2022-07-24 17:20:06 +0900

    src/roff/groff/tests/smoke-test_ps_device.sh: add a smoke test (#1,#2)

---
+++ src/roff/groff/tests/smoke-test_ps_device.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Copyright (C) 2022 Free Software Foundation, Inc.
+#
+# This file is part of groff.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+
+# test for PostScript with Japanese, preprocessor
+echo "testing -Kutf8 -Tps -Z" >&2
+printf ".ft JPM\nあ安" | "$groff" -Kutf8 -Tps -Z | tr '\n' ';' | grep -q 'Cu3042;h10000;Cu5B89;h10000;'
+
+
+# test for PostScript with Japanese, UTF16 encoding
+echo "testing -Kutf8 -Tps" >&2
+printf ".ft JPM\nあ安" | "$groff" -Kutf8 -Tps | grep -q '/Ryumin-Light-UniJIS-UTF16-H SF<30425B89>'
